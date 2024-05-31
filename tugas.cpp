ce7eadab8b6869f1a6cc7687acd3f9a2605e110b
Ali <aliaddamawi@gmail.com>
2024-05-31 20:37:40 +0300

    nambah baris baru

--- tugas.cpp
+++ tugas.cpp
@@ -8,4 +8,5 @@ int main()
     cout << "Ini fitur pada Fitur-tugascpp" << endl;
     cout << "Ini tambahan fitur pada master" << endl;
     cout << "tambahan fitur pada fitur-tugascpp" << endl;
+    cout << "baris baru" << endl;
 }
